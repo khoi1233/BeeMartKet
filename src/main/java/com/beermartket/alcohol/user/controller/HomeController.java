608451f20876b386a97557c9c7fcffcf6e5f1a72
khoi1233 <114672335+khoi1233@users.noreply.github.com>
2023-11-01 02:57:59 +0700

    trang
    làm xong menu trang

--- src/main/java/com/beermartket/alcohol/user/controller/HomeController.java
+++ src/main/java/com/beermartket/alcohol/user/controller/HomeController.java
@@ -17,28 +17,69 @@ import com.beermartket.alcohol.repository.HinhReponsitory;
 public class HomeController {
 	@Autowired
 	HinhReponsitory hinhDao;
-	
+
 	@RequestMapping("/home")
 	public String Home() {
-			
-		 return "customer/view/home/home";
-	}
-	
-	@GetMapping("/product_detail/{maSanPham}")
-	public String Product_detail(@PathVariable("maSanPham") int maSanPham, Model model) {
-	   
-		List<Hinh> hinhSanPhamHinh = hinhDao.findHinhByMaSanPham(maSanPham);
-		for(Hinh hinh : hinhSanPhamHinh ) {
-			System.out.println(hinh.getTenHinh());			
-		}	
-		model.addAttribute("hinh", hinhSanPhamHinh);
-		 return "customer/view/home/product-video";
-	}
-	
-	@RequestMapping("/faq")
-	public String Faq() {
-			
-		 return "customer/view/home/faq";
+
+		return "customer/view/home/home";
 	}
 
+	@GetMapping("/product_detail/{maSanPham}")
+	public String Product_detail(@PathVariable("maSanPham") int maSanPham, Model model) {
+
+		List<Hinh> hinhSanPhamHinh = hinhDao.findHinhByMaSanPham(maSanPham);
+		for (Hinh hinh : hinhSanPhamHinh) {
+
+		}
+		model.addAttribute("hinh", hinhSanPhamHinh);
+		return "customer/view/home/product-video";
+	}
+
+	@RequestMapping("/faq")
+	public String Faq() {
+
+		return "customer/view/home/faq";
+	}
+
+	@RequestMapping("/offerl")
+	public String Offerls() {
+
+		return "customer/view/home/offerl";
+	}
+
+	@RequestMapping("/profile")
+	public String Profile() {
+
+		return "customer/view/home/profile";
+	}
+	
+	@RequestMapping("/change-password")
+	public String ChangePassword() {
+
+		return "customer/view/home/change-password";
+	}
+	
+	@RequestMapping("/about")
+	public String About() {
+
+		return "customer/view/home/about";
+	}
+
+	@RequestMapping("/contact")
+	public String Contact() {
+
+		return "customer/view/home/contact";
+	}
+	
+	@RequestMapping("/privacy")
+	public String Privacy() {
+
+		return "customer/view/home/privacy";
+	}
+	
+	@RequestMapping("/error")
+	public String Email_en() {
+
+		return "customer/view/home/error";
+	}
 }
